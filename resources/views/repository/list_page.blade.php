8bba934ef482fb36c9b212b515571a29a0ac627c
hz300495saa <sidak.alexander@a-bank.com.ua>
2024-07-29 13:11:38 +0300

    Созданы фильтра для страниц дашборда и репозитория + переход по линке с описания

--- resources/views/repository/list_page.blade.php
+++ resources/views/repository/list_page.blade.php
@@ -4,7 +4,6 @@
 
     @include('layout.sidebar_nav')
 
-
     <div class="col">
 
         <div class="border-bottom my-3">
@@ -19,11 +18,14 @@
             </h3>
         </div>
 
+        <!-- Поле ввода для фильтрации -->
+        <div style="margin-bottom: 10px;">
+            <input type="text" id="filter-input" class="form-control" placeholder="Введите для фильтрации...">
+        </div>
 
-        <div class="row row-cols-3 g-3">
+        <div class="row row-cols-3 g-3" id="repository-list">
             @foreach($repositories as $repository)
-
-                <div class="col">
+                <div class="col repository-item">
                     <div class="base_block border h-100 shadow-sm rounded">
 
                         <div class="card-body">
@@ -34,7 +36,11 @@
 
                             @if($repository->description)
                                 <div class="card-text text-muted">
-                                    <span> {{$repository->description}} </span>
+                                    <span>{!! preg_replace(
+                                        '#\bhttps?://[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/))#',
+                                        '<a href="$0" target="_blank">$0</a>',
+                                        e($repository->description)
+                                    ) !!}</span>
                                 </div>
                             @endif
                         </div>
@@ -42,22 +48,36 @@
                         <div class="d-flex justify-content-end border-top p-2">
                             <span class="text-muted">
                                 <b>{{ $repository->suitesCount() }}</b> Test Suites
-                                 | <b>{{ $repository->casesCount() }}</b> Test Cases
-                                  | <b>{{ $repository->automatedCasesCount() }}</b> Automated
-                             </span>
+                                | <b>{{ $repository->casesCount() }}</b> Test Cases
+                                | <b>{{ $repository->automatedCasesCount() }}</b> Automated
+                            </span>
                         </div>
 
                     </div>
                 </div>
-
             @endforeach
         </div>
 
     </div>
 
-
-
-
 @endsection
 
+@section('footer')
+    <script>
+        document.getElementById('filter-input').addEventListener('input', function () {
+            let searchTerm = this.value.toLowerCase();
+            let repositoryItems = document.querySelectorAll('.repository-item');
 
+            repositoryItems.forEach(function (item) {
+                let repositoryTitle = item.querySelector('a.fs-4').textContent.toLowerCase();
+                let repositoryDescription = item.querySelector('.card-text span')?.textContent.toLowerCase() || '';
+
+                if (repositoryTitle.includes(searchTerm) || repositoryDescription.includes(searchTerm)) {
+                    item.style.display = '';
+                } else {
+                    item.style.display = 'none';
+                }
+            });
+        });
+    </script>
+@endsection
