0ec137d962a572e7748e1e0ec5d1972721081a39
hz300495saa <sidak.alexander@a-bank.com.ua>
2024-11-12 00:24:51 +0200

    fix
    * change order

--- resources/views/layout/sidebar_nav.blade.php
+++ resources/views/layout/sidebar_nav.blade.php
@@ -67,7 +67,7 @@
         <hr>
 
         <div class="text-center text-white mt-4">
-            <small>Version 3.17.9</small>
+            <small>Version 3.17.10</small>
         </div>
 
     </div>
